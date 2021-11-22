768e301fe5ebcd9277992ce6306b508b5af3e6d0
Herman <herman.randem@hotmail.com>
2021-11-23 00:25:42 +0100

    improved responsiveness of boxlist. Made boxlist its own component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import "./App.css";
 import Header from "./components/header.js";
 import Footer from "./components/footer.js";
+import BoxList from "./components/boxlist";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="container mx-auto my-20 flex-col space-y-8">
-        <h1 className="text-4xl text-secondary">Page heading</h1>
-        <TailwindCSSContainerBeige />
-        <TailwindCSSContainerBeige />
-        <TailwindCSSContainerWhite />
-        <TailwindCSSContainerBeige />
-        <TailwindCSSContainerBeige />
-        <TailwindCSSContainerPink />
-        <TailwindCSSContainerBeige />
-        <TailwindCSSContainerWhite />
-      </div>
+      <BoxList />
       <Footer />
     </div>
-  );
-}
-
-// Import these? Ask my homie sindre //
-
-function TailwindCSSContainerWhite() {
-  return (
-    <div className="p-44 max-w-sm mx-auto bg-white border-2 border-smashpink rounded-xl shadow-md"></div>
-  );
-}
-function TailwindCSSContainerPink() {
-  return (
-    <div className="p-44 max-w-sm mx-auto bg-smashpink border-2 border-white rounded-xl shadow-md"></div>
-  );
-}
-function TailwindCSSContainerBeige() {
-  return (
-    <div className="p-44 max-w-sm mx-auto bg-softbeige rounded-xl shadow-md"></div>
   );
 }
 
---
+++ src/components/boxlist.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const BoxList = () => {
+  return (
+    <main className="max-w-8xl flex mx-auto">
+      <div className="my-20 flex flex-col justify-center space-y-8">
+        <h1 className="text-4xl text-secondary">Page heading</h1>
+        <div className="flex flex-row flex-wrap justify-evenly gap-y-8 gap-x-8">
+          <TailwindCSSContainerBeige />
+          <TailwindCSSContainerBeige />
+          <TailwindCSSContainerWhite />
+          <TailwindCSSContainerBeige />
+          <TailwindCSSContainerBeige />
+          <TailwindCSSContainerPink />
+          <TailwindCSSContainerBeige />
+          <TailwindCSSContainerWhite />
+        </div>
+      </div>
+    </main>
+  );
+};
+function TailwindCSSContainerWhite() {
+  return (
+    <div className="container p-36 h- max-w-xs bg-white border-2 border-smashpink rounded-xl shadow-md"></div>
+  );
+}
+function TailwindCSSContainerPink() {
+  return (
+    <div className="container p-36 max-w-xs bg-smashpink border-2 border-white rounded-xl shadow-md"></div>
+  );
+}
+function TailwindCSSContainerBeige() {
+  return (
+    <div className="container p-36 max-w-xs bg-softbeige rounded-xl shadow-md"></div>
+  );
+}
+export default BoxList;
